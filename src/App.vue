8f275c3df0328259b8e1d3337d2b4a9aa1644bf7
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-02-06 18:41:12 +0100

    adaptada la pagina a español

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Casa</RouterLink>
       </nav>
     </div>
   </header>
