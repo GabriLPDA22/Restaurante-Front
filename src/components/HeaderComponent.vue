f77c8e37255176f430028c75a2f67e238947431f
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-02-25 19:11:20 +0100

    ahora si, que faltaba header mobile

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -63,10 +63,10 @@
       <nav class="header__nav" @click.stop>
         <ul>
           <li><router-link to="/menu" @click="closeMenu">Menu</router-link></li>
-          <li><router-link to="#" @click="closeMenu">Events</router-link></li>
-          <li><router-link to="#" @click="closeMenu">About Us</router-link></li>
+          <li><a>Events</a></li>
+          <li><router-link to="/about" @click="closeMenu">About Us</router-link></li>
           <li><router-link to="/reservation" @click="closeMenu">Reservation</router-link></li>
-          <li><router-link to="#" @click="closeMenu">Orders</router-link></li>
+          <li><router-link to="/cart" @click="closeMenu">Orders</router-link></li>
           <li><router-link to="/contact" @click="closeMenu">Contact Us</router-link></li>
         </ul>
       </nav>
