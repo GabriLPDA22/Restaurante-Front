0cd319ed4b055be4c607a0d0176603e71965f7ac
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-02-25 18:54:36 +0100

    router arreglado

--- src/components/InputTextGuest.vue
+++ src/components/InputTextGuest.vue
@@ -74,7 +74,7 @@
 
 <script lang="ts">
 import { ref, computed, watch, onMounted, onBeforeUnmount, defineComponent } from 'vue';
-import tableIcon from '@/assets/table.svg';
+import tableIcon from '../assets/svg/table.svg';
 import { useReservationStore } from '@/stores/reservationStore'; // Ruta corregida
 
 export default defineComponent({
--- src/router/index.ts
+++ src/router/index.ts
@@ -22,38 +22,6 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: ContactPage,
-    },
-    {
-      path: '/details',
-      name: 'details',
-      component: FoodDetail,
-    },
-    {
-      path: '/menu',
-      name: 'menu',
-      component: ViewMenu,
-    },
-    {
-      path: '/about',
-      name: 'AboutUs',
-      component: AboutUs,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
   routes,
   scrollBehavior() {
     return { top: 0 };
