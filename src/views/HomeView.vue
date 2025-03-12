3ff60a879098594d685f0c5775d41b14251c70ee
franvilla <a22950@svalero.com>
2025-03-12 21:30:39 +0000

    comiteo esto porque va

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,7 +17,7 @@ const chefSpecial = {
   description:
     "Creamy Arborio rice infused with a medley of wild mushrooms, Parmesan cheese, and a hint of truffle oil. This exquisite dish promises a symphony of flavors, meticulously crafted to elevate your dining experience. Immerse yourself in the taste of Italy, one delightful spoonful at a time.",
   price: "$24",
-  image: "/public/chef-special.png",
+  image: "/chef-special.png",
 };
 
 const navigateToCategory = (title: string, mappedCategory: string) => {
